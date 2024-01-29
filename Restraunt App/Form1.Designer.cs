77f8b02d65789d33414bb4ba353e9a4305375726
Ethan Travasso <flameboyepic@gmail.com>
2024-01-29 22:58:43 +0000

    Add project files.

---
+++ Restraunt App/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace Restraunt_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.starter1 = new System.Windows.Forms.ComboBox();
+            this.starter2 = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tb1 = new System.Windows.Forms.TrackBar();
+            this.tb2 = new System.Windows.Forms.TrackBar();
+            this.main1 = new System.Windows.Forms.ComboBox();
+            this.main2 = new System.Windows.Forms.ComboBox();
+            this.tb4 = new System.Windows.Forms.TrackBar();
+            this.tb3 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.dollarSign = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Mistral", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.Brown;
+            this.title.Location = new System.Drawing.Point(26, 24);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(269, 76);
+            this.title.TabIndex = 0;
+            this.title.Text = "Chef\'s Spice";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mistral", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(41, 300);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 76);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Menu";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Mistral", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(60, 414);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "- Nachos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Mistral", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(60, 509);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 22);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "- Burger";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Mistral", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(60, 436);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 22);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "- Fries";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Mistral", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.IndianRed;
+            this.label6.Location = new System.Drawing.Point(57, 471);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 38);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Main";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Mistral", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(60, 531);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 22);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "- Pizza";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Mistral", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(60, 553);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 22);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "- Steak";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mistral", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.IndianRed;
+            this.label2.Location = new System.Drawing.Point(57, 376);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 38);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Starters";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Mistral", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.IndianRed;
+            this.label9.Location = new System.Drawing.Point(362, 145);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(96, 38);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Starters";
+            // 
+            // logo
+            // 
+            this.logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+            this.logo.Location = new System.Drawing.Point(39, 118);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(196, 179);
+            this.logo.TabIndex = 10;
+            this.logo.TabStop = false;
+            // 
+            // starter1
+            // 
+            this.starter1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.starter1.FormattingEnabled = true;
+            this.starter1.Items.AddRange(new object[] {
+            "Nachos",
+            "Fries"});
+            this.starter1.Location = new System.Drawing.Point(477, 154);
+            this.starter1.Name = "starter1";
+            this.starter1.Size = new System.Drawing.Size(136, 21);
+            this.starter1.TabIndex = 11;
+            // 
+            // starter2
+            // 
+            this.starter2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.starter2.FormattingEnabled = true;
+            this.starter2.Items.AddRange(new object[] {
+            "Nachos",
+            "Fries"});
+            this.starter2.Location = new System.Drawing.Point(659, 154);
+            this.starter2.Name = "starter2";
+            this.starter2.Size = new System.Drawing.Size(136, 21);
+            this.starter2.TabIndex = 12;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Mistral", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.IndianRed;
+            this.label10.Location = new System.Drawing.Point(374, 328);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(72, 38);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Main";
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(477, 208);
+            this.tb1.Maximum = 5;
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(135, 45);
+            this.tb1.TabIndex = 14;
+            this.tb1.Value = 1;
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(659, 208);
+            this.tb2.Maximum = 5;
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(135, 45);
+            this.tb2.TabIndex = 15;
+            this.tb2.Value = 1;
+            // 
+            // main1
+            // 
+            this.main1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.main1.FormattingEnabled = true;
+            this.main1.Items.AddRange(new object[] {
+            "Burger",
+            "Pizza",
+            "Steak"});
+            this.main1.Location = new System.Drawing.Point(477, 342);
+            this.main1.Name = "main1";
+            this.main1.Size = new System.Drawing.Size(136, 21);
+            this.main1.TabIndex = 16;
+            // 
+            // main2
+            // 
+            this.main2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.main2.FormattingEnabled = true;
+            this.main2.Items.AddRange(new object[] {
+            "Burger",
+            "Pizza",
+            "Steak"});
+            this.main2.Location = new System.Drawing.Point(659, 342);
+            this.main2.Name = "main2";
+            this.main2.Size = new System.Drawing.Size(136, 21);
+            this.main2.TabIndex = 17;
+            // 
+            // tb4
+            // 
+            this.tb4.Location = new System.Drawing.Point(660, 391);
+            this.tb4.Maximum = 5;
+            this.tb4.Name = "tb4";
+            this.tb4.Size = new System.Drawing.Size(135, 45);
+            this.tb4.TabIndex = 18;
+            this.tb4.Value = 1;
+            // 
+            // tb3
+            // 
+            this.tb3.Location = new System.Drawing.Point(477, 391);
+            this.tb3.Maximum = 5;
+            this.tb3.Name = "tb3";
+            this.tb3.Size = new System.Drawing.Size(135, 45);
+            this.tb3.TabIndex = 19;
+            this.tb3.Value = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(838, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 92);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Calculate\r\nTotal";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(1004, 145);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(24, 25);
+            this.totalLabel.TabIndex = 21;
+            this.totalLabel.Text = "0";
+            this.totalLabel.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.totalLabel.Click += new System.EventHandler(this.totalLabel_Click);
+            // 
+            // dollarSign
+            // 
+            this.dollarSign.AutoSize = true;
+            this.dollarSign.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dollarSign.Location = new System.Drawing.Point(986, 145);
+            this.dollarSign.Name = "dollarSign";
+            this.dollarSign.Size = new System.Drawing.Size(24, 25);
+            this.dollarSign.TabIndex = 22;
+            this.dollarSign.Text = "$";
+            this.dollarSign.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1181, 637);
+            this.Controls.Add(this.dollarSign);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb3);
+            this.Controls.Add(this.tb4);
+            this.Controls.Add(this.main2);
+            this.Controls.Add(this.main1);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.starter2);
+            this.Controls.Add(this.starter1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.logo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Chef\'s Spice";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tb3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.ComboBox starter1;
+        private System.Windows.Forms.ComboBox starter2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TrackBar tb1;
+        private System.Windows.Forms.TrackBar tb2;
+        private System.Windows.Forms.ComboBox main1;
+        private System.Windows.Forms.ComboBox main2;
+        private System.Windows.Forms.TrackBar tb4;
+        private System.Windows.Forms.TrackBar tb3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label dollarSign;
+    }
+}
+
---
+++ Restraunt App/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restraunt_App
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int total = 0;
+            total += (starter1.Text != "Nachos") ? 0 : 8 * tb1.Value;//Needs to be capitalized
+            total += (starter1.Text != "Fries") ? 0 : 8 * tb1.Value;
+            
+            total += (starter2.Text != "Fries") ? 0 : 8 * tb2.Value;
+            total += (starter2.Text != "Nachos") ? 0 : 8 * tb2.Value;
+
+            total += (main1.Text != "Burger") ? 0 : 9 * tb3.Value;
+            total += (main1.Text != "Steak") ? 0 : 19 * tb3.Value;
+            total += (main1.Text != "Pizza") ? 0 : 4 * tb3.Value;
+
+            total += (main2.Text != "Burger") ? 0 : 9 * tb4.Value;
+            total += (main2.Text != "Steak") ? 0 : 19 * tb4.Value;
+            total += (main2.Text != "Pizza") ? 0 : 4 * tb4.Value;
+            //MessageBox.Show(total.ToString());
+            totalLabel.Text = total.ToString();
+
+            //total = 0;
+        }
+
+        private void totalLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
